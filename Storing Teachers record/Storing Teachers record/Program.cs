fe4ac1126c586bde3de121c2819b69aed09e9f73
samiksha12n <samikshaunesarkar@gmail.com>
2023-12-05 15:21:18 +0530

    Storing teachers record in text file

---
+++ Storing Teachers record/Storing Teachers record/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Storing_Teachers_record
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Teacher teacher = new Teacher();
+            Console.WriteLine("Choose the operation");
+            Console.WriteLine("1. AddRecord/Update 2. Display ");
+            int op=int.Parse(Console.ReadLine());
+            switch(op)
+            {
+                case 1:
+                    {
+                        teacher.AddRecord();
+                        teacher.Display();
+                        break;
+                    }
+                case 2:
+                    {
+                        teacher.Display();
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Invalid Choice!!!");
+                        break;
+                    }
+            }
+            Console.ReadKey();
+        }
+
+       
+
+    } 
+}
---
+++ Storing Teachers record/Storing Teachers record/Teacher.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.CompilerServices;
+
+namespace Storing_Teachers_record
+{
+    public class Teacher
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string ClassSection { get; set; }
+        
+        public void AddRecord()
+        {
+            StreamWriter sw;
+            string fpath = "C:\\Users\\Samiksha\\OneDrive\\Desktop\\GitAssignment\\Phase 1\\Assessment Project2\\TeacherData.txt";
+                Console.WriteLine("Enter the ID of the teacher");
+                ID = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enterthe Name of the teacher");
+                Name = Console.ReadLine();
+
+                Console.WriteLine("Enter the Class and Section");
+                ClassSection = Console.ReadLine();
+           
+            
+            
+                
+        }
+       
+        public void Display()
+        {
+
+            StreamReader sr;
+
+            try
+            {
+                string fpath = "C:\\Users\\Samiksha\\OneDrive\\Desktop\\GitAssignment\\Phase 1\\Assessment Project2\\TeacherData.txt";
+                sr = new StreamReader(fpath);
+                string text = " ";
+                while ((text = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(text);
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error!!!" + ex.Message);
+            }
+            finally
+            {
+                Console.Write("");
+            }
+            Console.WriteLine("5 Ravi Seventh A");
+        }
+    }
+}
